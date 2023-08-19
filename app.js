2506beb18fcc2cd7251c23b9da318404ff634e2d
Jack Sebben <jacksebben11@hotmail.com>
2023-08-18 23:13:11 -0400

    GameLogic module object can now perform the logic to create only 2 players (and no more) using the private _Player() factory function. This is done through the invocation of the createPlayer() method. This has been successfully done through the use of an HTML form and an event listener on the 'submit' button of the form, that will submit the player's input to the createPlayer() function. These player instances are stored in the players array property of the gamePlay module object.

--- app.js
+++ app.js
@@ -4,16 +4,44 @@ const gamePlay = (function(){ // Controls the flow of events and stores the stat
         ['O', 'X', 'O'],
         ['Y', 'X', 'X']
     ] ;
-    return { board }
+
+    let players = [] ; 
+
+    return { board, players }
 })() ;
 
 const gameLogic = (function() { // Performs the logic necessary to make changes to the game.
+    
+    const _Player = function(name, marker) {
+        function getName() { return name } ; 
+        function getMarker() { return marker } ;
+    
+        return { getName, getMarker } ;
+    }
+    
+    function createPlayer() {
+        if (gamePlay.players.length < 2) { // Makes sure we don't create more then 2 players
+            let name = document.getElementById('name').value;
+            let marker = document.querySelector('input[name="marker"]:checked').value;
+            let player = _Player(name, marker) ; 
+            gamePlay.players.push(player)
+            console.log('new player created')
 
+            if (gamePlay.players.length === 1) { // Re-renders the form for player 2 (1st marker taken = no more options)
+                // re-renderForm() for player 2
+                console.log('re-rendering form')
+            } else { console.log('removing form')// removes form entirely and commences the game
+            }
+
+        } else console.log('error')
+    }
+
+    return { createPlayer }
 })() ; 
 
-const gameBoard = (function(){ // Controls the elements and display rendering of the board.
+const gameDisplay = (function(){ // Controls the elements and rendering of the game in browser.
 
-    function render() {
+    function renderBoard() {
         let board = document.getElementById('board') ;
         document.querySelectorAll('.board-tile').forEach(tile => tile.remove());
 
@@ -26,15 +54,13 @@ const gameBoard = (function(){ // Controls the elements and display rendering of
             }
         }
     }
-    return { render } ;
+    return { renderBoard } ;
 })() ;
 
-const PlayerFactory = function(name, marker) {
-    function getName() { return name } ; 
-    function getMarker() { return marker } ;
-
-    return { getName, getMarker } ;
-}
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    gameLogic.createPlayer()
+})
 
 
 
