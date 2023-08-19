449a8a48ddb168f23be4fba6c6d2a7b3f4da0764
Jack Sebben <jacksebben11@hotmail.com>
2023-08-19 19:34:39 -0400

    Game can track when a player wins using the evaluateRoundWin() function in the gameLogic module object. This function will reset the board if all positions have been taken on the board. The board is then reset using a function in the gamePlay module object, newRound(), that resets the 'board' array to be empty and then re-renders the board by invoking the gameDisplay.renderBoard() method.
    Cleaned up the code and renamed some variables. Everything working.

--- app.js
+++ app.js
@@ -11,16 +11,37 @@ const gamePlay = (function(){ // Controls the flow of events and stores the stat
 
     let players = [] ; 
 
-    return { board, players, _turnCount }
+    function getBoard() {
+        return board
+    }
+
+    function newRound() {
+        board = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ] ;
+        
+        gameDisplay.renderBoard()
+    }
+
+    function newGame() {
+
+    }
+
+    return { board, getBoard, players, _turnCount, _roundCount, newRound, newGame }
 })() ;
 
+
+
 const gameLogic = (function() { // Performs the logic necessary to make changes to the game.
-    
+
     const _Player = function(name, marker) {
         function getName() { return name } ; 
         function getMarker() { return marker } ;
+        let wins = 0 ;
     
-        return { getName, getMarker } ;
+        return { getName, getMarker, wins } ;
     }
 
     function createPlayer() {
@@ -40,21 +61,50 @@ const gameLogic = (function() { // Performs the logic necessary to make changes 
         } else console.log('error')
     }
 
-    function whosTurnIsIt() {
+    function whosTurnItIs() {
         let playerTurn = null
 
         gamePlay._turnCount % 2 === 0 ? playerTurn = gamePlay.players[1] : playerTurn = gamePlay.players[0] ;
-
-        gamePlay._turnCount++
         return playerTurn
     }
 
-    function evaluateWin() {
+    function evaluateRoundWin() {
+        function getWinningPositions() { // Retrieves the specific win positions from current board.
+            return [
+                [ gamePlay.getBoard()[0][0], gamePlay.getBoard()[0][1], gamePlay.getBoard()[0][2] ],
+                [ gamePlay.getBoard()[1][0], gamePlay.getBoard()[1][1], gamePlay.getBoard()[1][2] ],
+                [ gamePlay.getBoard()[2][0], gamePlay.getBoard()[2][1], gamePlay.getBoard()[2][2] ],
+                [ gamePlay.getBoard()[0][0], gamePlay.getBoard()[1][0], gamePlay.getBoard()[2][0] ],
+                [ gamePlay.getBoard()[0][1], gamePlay.getBoard()[1][1], gamePlay.getBoard()[2][1] ],
+                [ gamePlay.getBoard()[0][2], gamePlay.getBoard()[1][2], gamePlay.getBoard()[2][2] ],
+                [ gamePlay.getBoard()[0][0], gamePlay.getBoard()[1][1], gamePlay.getBoard()[2][2] ],
+                [ gamePlay.getBoard()[0][2], gamePlay.getBoard()[1][1], gamePlay.getBoard()[2][0] ]
+            ]
+         } ;
 
+         if (gamePlay._turnCount === 10) {
+            gamePlay.newRound()
+            return 
+         }
+
+         for (let i = 0 ; i < getWinningPositions().length ; i++) {
+            if (getWinningPositions()[i].join() == [ "X", "X", "X" ].join()){
+                console.log('Player X wins') ;
+                whosTurnItIs().wins += 1 ;
+                break
+            }
+            if (getWinningPositions()[i].join() == ["O", "O", "O"].join()) {
+                console.log('Player O wins') ;
+                whosTurnItIs().wins += 1 ;
+                break
+            }
+        }
     }
 
-    return { createPlayer, whosTurnIsIt, evaluateWin }
+    return { createPlayer, whosTurnItIs, evaluateRoundWin  }
 })() ; 
+
+
 
 const gameDisplay = (function(){ // Controls the elements and rendering of the game in browser.
 
@@ -62,15 +112,20 @@ const gameDisplay = (function(){ // Controls the elements and rendering of the g
         let board = document.getElementById('board') ;
         document.querySelectorAll('.board-tile').forEach(tile => tile.remove()) ;
 
-        for (let row = 0 ; row < gamePlay.board.length ; row++ ) {
-            for (let column = 0 ; column < gamePlay.board[row].length ; column++) {
+        for (let row = 0 ; row < gamePlay.getBoard().length ; row++ ) {
+            for (let column = 0 ; column < gamePlay.getBoard()[row].length ; column++) {
                 let boardTile = document.createElement('div') ;
                 boardTile.className = 'board-tile' ;
-                boardTile.innerText = gamePlay.board[row][column] ; 
+                boardTile.innerText = gamePlay.getBoard()[row][column] ; 
+                boardTile.row = row
+                boardTile.column = column
 
                 boardTile.addEventListener('click', function(event) {
                     if (event.target.innerText === '') {
-                        event.target.innerText = gameLogic.whosTurnIsIt().getMarker()
+                        event.target.innerText = gameLogic.whosTurnItIs().getMarker()
+                        gamePlay.board[event.target.row][event.target.column] = gameLogic.whosTurnItIs().getMarker()
+                        gamePlay._turnCount++
+                        gameLogic.evaluateRoundWin()
                     }
                 }) ;
 
@@ -80,6 +135,8 @@ const gameDisplay = (function(){ // Controls the elements and rendering of the g
     }
     return { renderBoard } ;
 })() ;
+
+gameDisplay.renderBoard()
 
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault()
